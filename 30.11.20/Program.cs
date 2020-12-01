cb246b24c74a7dd61a8f92405bdccba3bbd2acf7
Mertbaba Okulmuş <51191427+MertbabaOkulmus@users.noreply.github.com>
2020-12-01 22:25:54 +0300

    Program.cs Updated

--- 30.11.20/Program.cs
+++ 30.11.20/Program.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using System.IO;
+using Microsoft.Azure.AppService.ApiApps.Service;
 
 namespace _30._11._20
 {
     class Program
     {
-        public const string clientId = "56606873-1d18-4a7d-b3c6-83f39c3a213a";
-        public static string[] Scopes = { "Files.ReadWrite.All" };
-        public static GraphServiceClient graphClient = null;
-        public static PublicClientApplication IdentityClientApp = new PublicClientApplication(clientId);
-        public static string TokenForUser = null;
-        public static DateTimeOffset Expiration;
+       
+        private enum ClientType
+        {
+            Consumer,
+            Business
+        }
         private ClientType clientType { get; set; }
+
+        public const string CLIENT_ID =     "*********************";
+        public const string CLIENT_SECRET = "********************";
+        public const string TENANT_ID =     "*************************";
+        public static string[] SCOPES = { "Files.ReadWrite.All" };
+        public static GraphServiceClient graphClient = null;
+       //public static string TokenForUser = null;
         private DriveItem CurrentFolder { get; set; }
-        private DriveItem SelectedItem { get; set; }
         public static GraphServiceClient GetAuthenticatedClient()
         {
             if (graphClient == null)
@@ -34,8 +41,8 @@ namespace _30._11._20
                             {
                                 var token = await GetTokenForUserAsync();
                                 requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
-                                // This header has been added to identify our sample in the Microsoft Graph service.  If extracting this code for your project please remove.
-                                requestMessage.Headers.Add("SampleID", "uwp-csharp-apibrowser-sample");
+                      // This header has been added to identify our sample in the Microsoft Graph service.  If extracting this code for your project please remove.
+                      requestMessage.Headers.Add("SampleID", "uwp-csharp-apibrowser-sample");
 
                             }));
                     return graphClient;
@@ -49,47 +56,28 @@ namespace _30._11._20
 
             return graphClient;
         }
-        public static  async Task<string> GetTokenForUserAsync()
+        public static async Task<string> GetTokenForUserAsync()
         {
-            AuthenticationResult authResult;
-            try
-            {
-                authResult = await IdentityClientApp.AcquireTokenSilentAsync(Scopes);
-                TokenForUser = authResult.IdToken;// token id token ile değiştirlidi 
-            }
+            var confidentialClient = ConfidentialClientApplicationBuilder
+                      .Create(CLIENT_ID)
+                      .WithAuthority($"https://login.microsoftonline.com/$TENANT_ID/v2.0")
+                      .WithClientSecret(CLIENT_SECRET)
+                      .Build();
 
-            catch (Exception)
-            {
-                if (TokenForUser == null || Expiration <= DateTimeOffset.UtcNow.AddMinutes(5))
-                {
-                    authResult = await IdentityClientApp.AcquireTokenAsync(Scopes);
+            var authResult = await confidentialClient
+                              .AcquireTokenForClient(SCOPES)
+                              .ExecuteAsync();
 
-                    TokenForUser = authResult.IdToken;
-                    Expiration = authResult.ExpiresOn;
-                }
-            }
-
-            return TokenForUser;
+            return authResult.AccessToken;
         }
 
-       public static System.IO.Stream GetFileStreamForUpload(string targetFolderName, out string originalFilename)
+        public static System.IO.Stream GetFileStreamForUpload(out string originalFilename)
         {
-           // OpenFileDialog dialog = new OpenFileDialog();
-           
-            //dialog.Title = "Upload to " + targetFolderName;
-            //dialog.Filter = "All Files (*.*)|*.*";
-            //dialog.CheckFileExists = true;
-            //var response = dialog.ShowDialog();
-            //if (response != DialogResult.OK)
-            //{
-            //    originalFilename = null;
-            //    return null;
-            //}
 
             try
-            {
-                originalFilename = System.IO.Path.GetFileName("");
-                return new System.IO.FileStream("", System.IO.FileMode.Open);
+            {/*C:\Users\mertb\OneDrive\Masaüstü\EnXwDWrXMAAyC7h.jfif*/
+                originalFilename = System.IO.Path.GetFileName("C:\\Users\\mertb\\OneDrive\\Masaüstü\\EnXwDWrXMAAyC7h.jfif");
+                return new System.IO.FileStream("C:\\Users\\mertb\\OneDrive\\Masaüstü\\EnXwDWrXMAAyC7h.jfif", System.IO.FileMode.Open);
             }
             catch (Exception ex)
             {
@@ -102,34 +90,135 @@ namespace _30._11._20
 
         public static async void Upload()
         {
-            var targetFolder = this.CurrentFolder;
+            var targetFolder = new Program().CurrentFolder;
             string filename;
-            using (var stream = GetFileStreamForUpload(targetFolder.Name, out filename))
+            using (var stream = GetFileStreamForUpload(out filename))
             {
                 if (stream != null)
                 {
-                    // Since the ItemWithPath method is available only at Drive.Root, we need to strip
-                    // /drive/root: (12 characters) from the parent path string.
                     string folderPath = targetFolder.ParentReference == null
                         ? ""
-                        : targetFolder.ParentReference.Path.Remove(0, 12) + "/" + Uri.EscapeUriString(targetFolder.Name);
+                        : targetFolder.ParentReference.Path.Remove(0, 12) + "/" + Uri.EscapeUriString("deneme");
                     var uploadPath = folderPath + "/" + Uri.EscapeUriString(System.IO.Path.GetFileName(filename));
 
-                    
-                        var uploadedItem =
-                            await
-                               graphClient.Drive.Root.ItemWithPath(uploadPath).Content.Request().PutAsync<DriveItem>(stream);
 
-                        Console.WriteLine("Uploaded with ID: " + uploadedItem.Id);
-                    
+                    var uploadedItem =
+                        await
+                           graphClient.Drive.Root.ItemWithPath(uploadPath).Content.Request().PutAsync<DriveItem>(stream);
+
+                    Console.WriteLine("Uploaded with ID: " + uploadedItem.Id);
+
                 }
             }
         }
         static void Main(string[] args)
         {
-            graphClient =GetAuthenticatedClient();
+            graphClient = GetAuthenticatedClient();
+            var users = graphClient.Users
+            .Request()
+            .GetAsync().GetAwaiter();
+
             Upload();
             Console.ReadKey();
         }
+
+        //---------------------------------------
+        public async Task SignIn()
+        {
+
+            try
+            {
+                graphClient = GetAuthenticatedClient();
+            }
+            catch (ServiceException exception)
+            {
+
+                PresentServiceException(exception);
+
+            }
+
+            try
+            {
+                await LoadFolderFromPath();
+
+               // UpdateConnectedStateUx(true);
+            }
+            catch (ServiceException exception)
+            {
+                PresentServiceException(exception);
+                graphClient = null;
+            }
+        }
+        public async Task LoadFolderFromPath(string path = null)
+        {
+            if (null == graphClient) return;
+
+            // Update the UI for loading something new
+            
+
+            try
+            {
+                DriveItem folder;
+
+                var expandValue = this.clientType == ClientType.Consumer
+                    ? "thumbnails,children($expand=thumbnails)"
+                    : "thumbnails,children";
+
+                if (path == null)
+                {
+                    folder = await graphClient.Drive.Root.Request().Expand(expandValue).GetAsync();
+                }
+                else
+                {
+                    folder =
+                        await
+                                 graphClient.Drive.Root.ItemWithPath("/" + path)
+                                .Request()
+                                .Expand(expandValue)
+                                .GetAsync();
+                }
+
+                ProcessFolder(folder);
+            }
+            catch (Exception exception)
+            {
+                PresentServiceException(exception);
+            }
+
+            
+        }
+
+       
+
+        private static void PresentServiceException(Exception exception)
+        {
+            string message = null;
+            var oneDriveException = exception as ServiceException;
+            if (oneDriveException == null)
+            {
+                message = exception.Message;
+            }
+            else
+            {
+                message = string.Format("{0}{1}", Environment.NewLine, oneDriveException.ToString());
+            }
+
+            Console.WriteLine(string.Format("OneDrive reported the following error: {0}", message));
+        }
+        private void ProcessFolder(DriveItem folder)
+        {
+            if (folder != null)
+            {
+                this.CurrentFolder = folder;
+
+                LoadProperties(folder);
+
+            }
+        }
+        private void LoadProperties(DriveItem item)
+        {
+            
+        }
+
     }
 }
