5fb0012e6a5091cd2a90db5aeccc0428d49ac001
Mertbaba Okulmuş <51191427+MertbabaOkulmus@users.noreply.github.com>
2020-12-02 00:52:31 +0300

    Program.cs Updated

--- 30.11.20/Program.cs
+++ 30.11.20/Program.cs
@@ -6,27 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using System.IO;
-using Microsoft.Azure.AppService.ApiApps.Service;
 
 namespace _30._11._20
 {
     class Program
     {
-       
-        private enum ClientType
-        {
-            Consumer,
-            Business
-        }
-        private ClientType clientType { get; set; }
-
         public const string CLIENT_ID =     "*********************";
         public const string CLIENT_SECRET = "********************";
         public const string TENANT_ID =     "*************************";
         public static string[] SCOPES = { "Files.ReadWrite.All" };
         public static GraphServiceClient graphClient = null;
-       //public static string TokenForUser = null;
         private DriveItem CurrentFolder { get; set; }
+
+        // Verilen bağlam ve kaynak kimliği için bir erişim belirteci alın. İlk önce bir girişimde bulunulur
         public static GraphServiceClient GetAuthenticatedClient()
         {
             if (graphClient == null)
@@ -41,8 +33,7 @@ namespace _30._11._20
                             {
                                 var token = await GetTokenForUserAsync();
                                 requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
-                      // This header has been added to identify our sample in the Microsoft Graph service.  If extracting this code for your project please remove.
-                      requestMessage.Headers.Add("SampleID", "uwp-csharp-apibrowser-sample");
+                    //  requestMessage.Headers.Add("SampleID", "uwp-csharp-apibrowser-sample");
 
                             }));
                     return graphClient;
@@ -56,6 +47,9 @@ namespace _30._11._20
 
             return graphClient;
         }
+
+
+        //Kullanıcı için Token alındığı kısım
         public static async Task<string> GetTokenForUserAsync()
         {
             var confidentialClient = ConfidentialClientApplicationBuilder
@@ -67,15 +61,16 @@ namespace _30._11._20
             var authResult = await confidentialClient
                               .AcquireTokenForClient(SCOPES)
                               .ExecuteAsync();
-
             return authResult.AccessToken;
         }
 
+
+        // Yükleme İçin Dosyanın path bilgilerini  alındığı kısım
         public static System.IO.Stream GetFileStreamForUpload(out string originalFilename)
         {
 
             try
-            {/*C:\Users\mertb\OneDrive\Masaüstü\EnXwDWrXMAAyC7h.jfif*/
+            {   /*C:\Users\mertb\OneDrive\Masaüstü\deneme.pdf*/
                 originalFilename = System.IO.Path.GetFileName("C:\\Users\\mertb\\OneDrive\\Masaüstü\\EnXwDWrXMAAyC7h.jfif");
                 return new System.IO.FileStream("C:\\Users\\mertb\\OneDrive\\Masaüstü\\EnXwDWrXMAAyC7h.jfif", System.IO.FileMode.Open);
             }
@@ -86,7 +81,6 @@ namespace _30._11._20
                 return null;
             }
         }
-
 
         public static async void Upload()
         {
@@ -118,107 +112,9 @@ namespace _30._11._20
             .Request()
             .GetAsync().GetAwaiter();
 
-            Upload();
+            //Upload();
             Console.ReadKey();
-        }
-
-        //---------------------------------------
-        public async Task SignIn()
-        {
-
-            try
-            {
-                graphClient = GetAuthenticatedClient();
-            }
-            catch (ServiceException exception)
-            {
-
-                PresentServiceException(exception);
-
-            }
-
-            try
-            {
-                await LoadFolderFromPath();
-
-               // UpdateConnectedStateUx(true);
-            }
-            catch (ServiceException exception)
-            {
-                PresentServiceException(exception);
-                graphClient = null;
-            }
-        }
-        public async Task LoadFolderFromPath(string path = null)
-        {
-            if (null == graphClient) return;
-
-            // Update the UI for loading something new
-            
-
-            try
-            {
-                DriveItem folder;
-
-                var expandValue = this.clientType == ClientType.Consumer
-                    ? "thumbnails,children($expand=thumbnails)"
-                    : "thumbnails,children";
-
-                if (path == null)
-                {
-                    folder = await graphClient.Drive.Root.Request().Expand(expandValue).GetAsync();
-                }
-                else
-                {
-                    folder =
-                        await
-                                 graphClient.Drive.Root.ItemWithPath("/" + path)
-                                .Request()
-                                .Expand(expandValue)
-                                .GetAsync();
-                }
-
-                ProcessFolder(folder);
-            }
-            catch (Exception exception)
-            {
-                PresentServiceException(exception);
-            }
-
-            
-        }
-
-       
-
-        private static void PresentServiceException(Exception exception)
-        {
-            string message = null;
-            var oneDriveException = exception as ServiceException;
-            if (oneDriveException == null)
-            {
-                message = exception.Message;
-            }
-            else
-            {
-                message = string.Format("{0}{1}", Environment.NewLine, oneDriveException.ToString());
-            }
-
-            Console.WriteLine(string.Format("OneDrive reported the following error: {0}", message));
-        }
-        private void ProcessFolder(DriveItem folder)
-        {
-            if (folder != null)
-            {
-                this.CurrentFolder = folder;
-
-                LoadProperties(folder);
-
-            }
-        }
-        private void LoadProperties(DriveItem item)
-        {
-            
-        }
+        }       
 
     }
 }
